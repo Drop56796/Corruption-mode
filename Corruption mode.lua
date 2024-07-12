4b9c15f726f1aa0adce35dd6b329d355f6c7f150
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-07-12 20:47:49 +0800

    Create Corruption mode.lua

---
+++ Corruption mode.lua
@@ -0,0 +1,9 @@
+-- detph
+coroutine.wrap(function()
+    while true do
+        wait(math.random(750,900))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(0.5)
+        
+    end
+end)()
