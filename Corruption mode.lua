7477b9c22adbe0e3a8870aa786f837ff28f947ba
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-07-13 09:04:50 +0800

    Update Corruption mode.lua

--- Corruption mode.lua
+++ Corruption mode.lua
@@ -1,9 +1,18 @@
--- detph
+-- Hungerd
 coroutine.wrap(function()
     while true do
         wait(math.random(750,900))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         wait(0.5)
-        
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Corruption-mode/main/hunger.lua"))()
+    end
+end)()
+
+--Detph
+coroutine.wrap(function()
+    while true do
+        wait(math.random(700,800))
+        wait(0.5)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Corruption-mode/main/enity.lua"))()
     end
 end)()
